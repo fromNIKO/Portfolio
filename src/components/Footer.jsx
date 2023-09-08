65925cfc57f94e832d427c90f5d05ee0020a25c8
BAKA <nicolasdevfs@gmail.com>
2023-09-08 10:53:01 +0200

    changement lien github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <>
-            <footer>
-                <div className="container-p">
-                <p>Développé par Nicolas Torres</p>
-                </div>
-                <div className="social-end">
-                <a href="https://www.linkedin.com/in/nicolas-torres-a72355218/">
-                    <i className="fa-brands fa-linkedin-in">
-                </i>
-                </a>
+  return (
+    <>
+      <footer>
+        <div className="container-p">
+          <p>Développé par Nicolas Torres</p>
+        </div>
+        <div className="social-end">
+          <a href="https://www.linkedin.com/in/nicolas-torres-a72355218/">
+            <i className="fa-brands fa-linkedin-in"></i>
+          </a>
 
-                <a href="https://github.com/fromNicolas">
-                <i className="fa-brands fa-github">
-                </i>
-                </a>
-                </div>
-            </footer>
-        </>
-    );
+          <a href="https://github.com/fromNIKO">
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
